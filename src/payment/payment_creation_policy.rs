5d963938fb492527ea999767f325899b9190fa55
Prem Pratap Singh Tomar <prem.tomar.gwl@gmail.com>
2026-09-03 21:19:58 +0530

    Checkpoint-Day-44-policy-constructor-attempt

--- src/payment/payment_creation_policy.rs
+++ src/payment/payment_creation_policy.rs
@@ -1,3 +1,7 @@
+use paykit_money::{Currency, PaymentAmount};
+
+use crate::PaymentMethodType;
+
 pub enum PaymentCreationPolicyRejection<A, C, M> {
     Amount(A),
     Currency(C),
@@ -9,4 +13,14 @@ pub struct PaymentCreationPolicySet<A, C, M> {
     amount_policy: A,
     currency_policy: C,
     payment_method_policy: M,
+}
+
+impl PaymentCreationPolicySet<PaymentAmount, Currency, PaymentMethodType> {
+    pub fn new(amount: Paym, currency: Currency, method_type: PaymentMethodType) -> Self {
+        Self {
+            amount_policy: amount,
+            currency_policy: currency,
+            payment_method_policy: method_type,
+        }
+    }
 }
